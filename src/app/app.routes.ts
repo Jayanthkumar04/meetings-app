89645716d82ec76b102e1b1c60733d3314b690ed
jayanth <jayanth.yadavalli@ascendion.com>
2024-12-02 20:44:45 +0530

    created features folder under it created calender and menu components and next work is child routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { LoginComponent } from './users/login/login.component';
 import { RegisterComponent } from './users/register/register.component';
+import { MenuComponent } from './features/menu/menu.component';
 
 export const routes: Routes = [
   {
@@ -19,5 +20,10 @@ export const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
     title: 'Registration Page',
+  },
+  {
+    path: 'features',
+    component: MenuComponent,
+    title: 'Features to update',
   },
 ];
--- src/app/features/menu/menu.component.ts
+++ src/app/features/menu/menu.component.ts
@@ -1,12 +1,12 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { NgModel, FormsModule, NgForm } from '@angular/forms';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [NgbModule, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, NgbCollapse],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss',
 })
